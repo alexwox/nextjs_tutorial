537f33224dd7041fb192d2e1941f77d63d3c4298
alexwox <alexander.woxstrom@gmail.com>
2024-09-07 16:19:06 +0200

    Chapter 13 done

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -16,6 +16,7 @@ const FormSchema = z.object({
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
+
     const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
@@ -25,15 +26,19 @@ export async function createInvoice(formData: FormData) {
 
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
-
-    await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
+    try {
+      await sql`
+      INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
   console.log('Database updated');
+  
+  } catch (error) {
+    return { message: 'Database Error: Failed to create invoices' };
+  }
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
-  }
+}
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
@@ -45,18 +50,27 @@ export async function updateInvoice(id: string, formData: FormData) {
     });
    
     const amountInCents = amount * 100;
-   
+    try {
     await sql`
       UPDATE invoices
       SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
       WHERE id = ${id}
     `;
    
-    revalidatePath('/dashboard/invoices');
-    redirect('/dashboard/invoices');
+  } catch (error) {
+    return { message: 'Database Error: Failed to update invoices' };
   }
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
 
-  export async function deleteInvoice(id: string) {
+export async function deleteInvoice(id: string) {
+    throw new Error('Failed to Delete Invoice');
+
+    try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
+  } catch (error) {
+    return { message: 'Database Error: Failed to delete invoices' };
   }
+}
